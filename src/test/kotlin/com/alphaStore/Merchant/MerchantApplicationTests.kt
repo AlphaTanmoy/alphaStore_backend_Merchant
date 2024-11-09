21acc235680338e4006dddf8a7a603bef7d205af
AlphaTanmoy <dtanmoy169@gmail.com>
2024-11-10 00:32:36 +0530

    update

---
+++ src/test/kotlin/com/alphaStore/Merchant/MerchantApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alphaStore.Merchant
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MerchantApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
