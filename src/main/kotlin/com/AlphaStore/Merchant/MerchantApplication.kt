7aa176433285918ce9657454736fec798370758e
AlphaTanmoy <dtanmoy169@gmail.com>
2024-11-10 10:33:38 +0530

    main file uploaded

--- src/main/kotlin/com/AlphaStore/Merchant/MerchantApplication.kt
+++ src/main/kotlin/com/AlphaStore/Merchant/MerchantApplication.kt
@@ -2,8 +2,10 @@ package com.alphaStore.Merchant
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class MerchantApplication
 
 fun main(args: Array<String>) {
